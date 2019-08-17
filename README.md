2a98970b605356843c9627edac5eaeb78449fe45
Nzix <iminezn5656@outlook.com>
2019-08-17 17:38:44 +0800

    commit all

---
+++ README.md
@@ -0,0 +1,21 @@
+# JikeRedirect-Xposed
+
+~~一个护眼 Xposed 模块~~
+
+## Feature
+
+- 连接 Jellow API
+
+- 移除 Webview 的外部链接重定向 (因为 `redirect.jellow.club` 坏掉了)
+
+- 强制显示招呼和通知 (聊天页 503)
+
+## Screenshot
+
+<img src="https://user-images.githubusercontent.com/26399680/63209559-1f976400-c115-11e9-82fe-6cbeef580d04.jpg" width="50%"/><img src="https://user-images.githubusercontent.com/26399680/63209558-1f976400-c115-11e9-85b6-494fb00cd81a.jpg" width="50%"/>
+
+> 没去 QAQ
+
+## License
+
+This project is under the GNU General Public License v3.0
---
+++ app/src/main/java/test/nondanee/jikeredirect/XposedInit.java
@@ -0,0 +1,102 @@
+package test.nondanee.jikeredirect;
+
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.net.URLDecoder;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class XposedInit implements IXposedHookLoadPackage {
+
+    final static String TAG = "nondanee.jike.redirect";
+
+    private String getViewName (View view){
+        int resourceId = view.getId();
+        if (resourceId == View.NO_ID) return "";
+        else return view.getContext().getResources().getResourceEntryName(resourceId);
+    }
+
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+        if (!loadPackageParam.packageName.equals("com.ruguoapp.jike")) return;
+
+        XposedBridge.log("Jike Redirect Start");
+
+        XposedHelpers.findAndHookMethod(
+            "com.ruguoapp.jike.network.d",
+            loadPackageParam.classLoader,
+            "a",
+            String.class,
+            new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    String host = (String) param.args[0];
+                    param.args[0] = host.replace("app.jike.ruguoapp.com", "api.jellow.club");
+                    super.beforeHookedMethod(param);
+                }
+            }
+        );
+
+        XposedHelpers.findAndHookMethod(
+            "com.ruguoapp.jike.network.d",
+            loadPackageParam.classLoader,
+            "c",
+            String.class,
+            new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    String url = (String) param.args[0];
+                    param.args[0] = url.replace("jike.ruguoapp.com", "jellow.club");
+                    super.beforeHookedMethod(param);
+                }
+            }
+        );
+
+        XposedHelpers.findAndHookMethod(
+            "com.ruguoapp.jike.global.h",
+            loadPackageParam.classLoader,
+            "b",
+            String.class,
+            new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    String uri = (String) param.args[0];
+                    if (!uri.startsWith("jike://page.jk/web?url=")) return;
+                    uri = uri.substring(23);
+                    uri = URLDecoder.decode(uri, "utf-8");
+                    if (!uri.startsWith("https://redirect.jike.ruguoapp.com/?redirect=")) return;
+                    param.args[0] = "jike://page.jk/web?url=" + uri.substring(45);
+                    super.beforeHookedMethod(param);
+                }
+            }
+        );
+
+        XposedHelpers.findAndHookMethod(
+            View.class,
+            "setVisibility",
+            int.class,
+            new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    View view = (View) param.thisObject;
+                    if (getViewName(view).isEmpty() && view instanceof ViewGroup) {
+                        for (int index = 0; index < ((ViewGroup)view).getChildCount(); index ++) {
+                            View childView = ((ViewGroup)view).getChildAt(index);
+                            if (getViewName(childView).equals("layNotificationEntryContainer")) {
+                                param.args[0] = View.VISIBLE;
+                                break;
+                            }
+                        }
+                    }
+                    super.beforeHookedMethod(param);
+                }
+            }
+        );
+    }
+}
